6fb8316592c3143570b3e9de5552c5086403aaa8
MYamashii <m.yamashii@gmail.com>
2019-04-25 08:54:25 +0800

    Update test_sample_add.py to pass test

--- hello/tests/test_sample_add.py
+++ hello/tests/test_sample_add.py
@@ -3,4 +3,4 @@ from django.test import TestCase
 
 class SmokeTest(TestCase):
     def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)  # 失敗用テスト
+        self.assertEqual(1 + 1, 2)  # サンプルテスト
