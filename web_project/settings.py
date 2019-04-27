a768d8eb7d6a48bf8b82808c31cc9042802041bb
Masato Yamashita <masatoyamashita@MBA.local>
2019-04-27 16:06:32 +0800

    Modified default Database statement.

--- web_project/settings.py
+++ web_project/settings.py
@@ -74,18 +74,19 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    #    'default': {
-    #        'ENGINE': 'django.db.backends.sqlite3',
-    #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #    }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sample',
-        'USER': 'root',
-        'HOST': '',
-        'PORT': '',
-     }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'sample',
+#        'USER': 'root',
+#        'HOST': '',
+#        'PORT': '',
+#     }
+# }
 
 
 # Password validation
