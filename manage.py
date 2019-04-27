8f8d2748bd33e97fdbd244ef67ba0afc7c376b5d
Masato Yamashita <masatoyamashita@MBA.local>
2019-04-27 15:58:14 +0800

    Deleted mysql import statement.

--- manage.py
+++ manage.py
@@ -2,12 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import pymysql
 
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_project.settings')
-    pymysql.install_as_MySQLdb()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
