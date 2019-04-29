cecbaf867b7dd3110c515b56498306980904cc27
Masato Yamashita <masatoyamashita@MBA.local>
2019-04-30 05:17:48 +0800

    Modified blog display page to display title, published, and summary title. #1

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 def home(request):
-    return render(request, "posts/index.html")
+    posts = Post.objects.order_by('-published')
+    return render(request, "posts/index.html", {"posts": posts})
+
+
+# def index(request):
+    # posts = Post.objects.order_by('-published')
+    # 
+    # return render(request, "posts/index.html", {"num": range(10)},)
